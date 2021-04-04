f36bb7e8cdc3aa47382bee8be67f9b97aba33e16
Hassan Habib <hassanhabib@live.com>
2021-04-04 15:10:00 -0700

    Add project files.

---
+++ PrettyBlazorDemo/Pages/Index.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace PrettyBlazorDemo.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public List<int> Numbers { get; set; } =
+            Enumerable.Range(start: 0, count: 10).ToList();
+    }
+}
